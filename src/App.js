c8a14e6fd62b189476a856a047e203fb29cf5d48
Phillips <DPhillips@ad.polysystems.com>
2020-03-11 09:18:15 -0500

    First Commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React, {useState, useEffect } from 'react';
+import Pages from './pages';
+import './App.css';
+
+function App() {
+  const [user, setUser] = useState(null);
+  useEffect(() => {
+    fetch('https://gitconnected.com/v1/portfolio/dphilli3')
+    .then(res => res.json())
+    .then(user => {
+      setUser(user);
+    });
+  
+  }, []);
+
+  if (!user) {
+    return <div />
+  }
+
+  return <Pages user={user} />;
+}
+
+export default App;
